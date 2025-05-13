384d2a4824309a783efd386e65b04d717c3418c2
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2025-05-13 13:16:59 +0800

    chore: configure production build for React

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const SKIP_BASE_PATH = env.VITE_SKIP_BASE_PATH
+
+  return {
+    plugins: [react()],
+    base: SKIP_BASE_PATH === 'true' ? '/' : '/dtr.gvs/',
+    publicDir: 'assets',
+    server: {
+      host: 'localhost',
+      port: 5000,
+      strictPort: true
+    },
+    build: {
+      outDir: 'public'
+    }
+  }
 })
